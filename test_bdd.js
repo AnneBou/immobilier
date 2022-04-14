d3a12a096abb1774f8ed485f6d916cd0b5daba60
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-04-14 09:48:18 +0200

    Gitignore

--- test_bdd.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(
-    'mongodb+srv://Anne:NV42cuzjq3lRN044@cluster0.rrzkr.mongodb.net/immo', 
-    {connectTimeoutMS : 3000, socketTimeoutMS: 20000, useNewUrlParser: true, useUnifiedTopology: true }
-);
-const db = mongoose.connection;
-db.once('open', () => {
-   console.log(`connexion OK !`);
-});
