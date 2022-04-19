ae6bb35749c4c0f2a2794b6110e3af7dbf407326
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-04-19 14:35:19 +0200

    Tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,29 @@
 const assert = require('assert');
+const { getMaxListeners } = require('process');
 it('simple test', (done) => {
   assert.ok(true);
   done();
+});
+
+describe('Utilisateurs', () => {
+    
+  // it(`Vérification email, d'un non-existant`, (done) => {
+  //     User.emailExists(User.email).then((result) => {
+  //         assert.equal(result, false);
+  //         done();
+  //     });
+  // });
+
+  it(`Création d'un compte`, () => {
+      assert.ok(true);
+  });
+
+/*    
+  it(`Vérification email, d'un existant`, (done) => {
+      repo.emailExists(entity.email).then((result) => {
+    
+      assert.ok(true);
+      done();
+  });
+*/
 });
