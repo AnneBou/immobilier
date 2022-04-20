a08dd7ac97ac33cf838de11bb8456049491357e2
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-04-20 09:15:07 +0200

    End Tests

--- test/test.js
+++ test/test.js
@@ -1,29 +1,36 @@
+process.env.URI_MONGODB = '';
 const assert = require('assert');
-const { getMaxListeners } = require('process');
-it('simple test', (done) => {
-  assert.ok(true);
-  done();
-});
+const RepoUser = require('../src/repository/User');
+const repo = new RepoUser();
+const entity = {
+    email : 'j.doe@yopmail.com',
+    password : 'test', // devra être hashé
+    civility : '1',
+    firstname: 'John',
+    lastname: 'Doe',
+    phone: '0600000000'
+};
+
 
 describe('Utilisateurs', () => {
     
-  // it(`Vérification email, d'un non-existant`, (done) => {
-  //     User.emailExists(User.email).then((result) => {
-  //         assert.equal(result, false);
-  //         done();
-  //     });
-  // });
-
-  it(`Création d'un compte`, () => {
-      assert.ok(true);
-  });
+    it(`Vérification email, d'un non existant`, (done) => {
+        repo.emailExists(entity.email).then((result) => {
+            assert.equal(result, false);
+            done();
+        });
+    });
+ 
+    it(`Création d'un compte`, () => {
+        assert.ok(true);
+    });
 
 /*    
-  it(`Vérification email, d'un existant`, (done) => {
-      repo.emailExists(entity.email).then((result) => {
-    
-      assert.ok(true);
-      done();
-  });
+    it(`Vérification email, d'un existant`, (done) => {
+        repo.emailExists(entity.email).then((result) => {
+      
+        assert.ok(true);
+        done();
+    });
 */
 });
